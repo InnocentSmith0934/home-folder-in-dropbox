9834a6f17a5c9e7ef445bad714516542ffc815d3
Smith, Innocent <InnocentSmith0934@users.noreply.github.com>
2014-04-25 20:31:29 -0500

    Create home_folder_dropbox
    Initial test form to see if I'm building the folder path correctly.

---
+++ home_folder_dropbox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Moves home folder files into the Dropbox folder
+# Assumes it being used with Dropbox for Business with the new folder structure
+# Assumes Dropbox is already linked, located in home folder
+# If using for plain Dropbox account, remove the third argument and its references
+# Takes three arguments: [initials] [shortname] [org name]
+# Needs to be called with superuser privs
+
+NEWHOME=\~/Dropbox\ \("$3"\)/"$1"/"$2"
+
+echo $NEWHOME
+echo "$NEWHOME"
+
+mkdir -p "$NEWHOME"
+
+exit 0
